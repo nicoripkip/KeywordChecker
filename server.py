be8af5d16ed4239324c132801167be6ef1a0bc3e
nicoripkip <nicovanommen.nvo@gmail.com>
2021-04-19 14:21:02 +0200

    Opzet flask project

---
+++ server.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+
+@app.route('/')
+def hello():
+    return "hellow world!"
+
+
+if __name__ == '__main__':
+    app.run()
